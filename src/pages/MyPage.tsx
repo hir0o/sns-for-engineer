2865d97a5242a09a8a1c4680f316e7db6c4013d3
hir0o <mk.9.q.kyu@gmail.com>
2021-08-17 02:41:24 +0900

    なんとなくユーザー登録

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,14 +1,66 @@
-import React, { VFC } from 'react'
+import React, { useCallback, VFC } from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
 import styled from 'styled-components'
 import useIp from '../hooks/useIp'
+import useLocalStorage from '../hooks/useLocalStorage'
+import { User } from '../types'
 
 const MyPage: VFC = () => {
   const ip = useIp()
+  const [user, setUser] = useState(
+    {} as Pick<User, 'name' | 'description' | 'id'>
+  )
+  const { get, set } = useLocalStorage('123user')
+  useEffect(() => {
+    setUser(get())
+  }, [])
+
+  const [name, setName] = useState('')
+  const [description, setDescription] = useState('')
+
+  const hunleSubmit = useCallback(async () => {
+    const res = await fetch(
+      'https://versatileapi.herokuapp.com/api/user/create_user',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          name,
+          description,
+        }),
+      }
+    )
+    set({ name, description })
+    console.log(res)
+  }, [name, description, set])
 
   return (
     <StyledMyPage>
       <p>mypage</p>
       <h1>IP: {ip}</h1>
+      <h1>ID: {user?.id}</h1>
+      <h1>name: {user?.name}</h1>
+      <h1>description: {user?.description}</h1>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        type="text"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button
+        onClick={() => {
+          hunleSubmit()
+        }}
+      >
+        送信
+      </button>
     </StyledMyPage>
   )
 }
