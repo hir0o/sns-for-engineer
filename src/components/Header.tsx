6345c548131f6ce7bc20742fa3fa5ddc99473179
hir0o <mk.9.q.kyu@gmail.com>
2021-08-20 11:34:52 +0900

    ヘッダーのレイアウトを整える

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,15 +2,22 @@ import React, { VFC } from 'react'
 import styled from 'styled-components'
 import ContentContainer from './ContentContainer'
 import { Link } from 'react-router-dom'
+import ProfileImage from "../components/ProfileImage"
+import useLocalStrage from "../hooks/useLocalStorage"
+import { User } from "../types"
 
 const Header: VFC = () => {
+  const [user, _] = useLocalStrage<User>("123user")
+
   return (
     <StyledHeader>
       <ContentContainer className="header__inner">
-        <h1>
+        <h1 className="header__title">
           <Link to="/">SNS for Engenner</Link>
         </h1>
-        <Link to="/mypage">mypage</Link>
+        <Link to="/mypage">
+          {user && <ProfileImage userId={user.id} size={30} />}
+        </Link>
       </ContentContainer>
     </StyledHeader>
   )
@@ -19,10 +26,14 @@ const Header: VFC = () => {
 const StyledHeader = styled.header`
   box-shadow: 0 3px 25px rgba(77, 77, 99, 0.1);
   background-color: #fff;
+  padding: 12px 0;
   .header__inner {
     display: flex;
     justify-content: space-between;
     align-items: center;
+  }
+  .header__title {
+    font-size: 16px;
   }
 `
 
